bdd2ee5f06cc0a2ef303e60c37c12a58ffe9d755
sadik182 <sadikurrahman728@gmail.com>
2022-10-10 23:22:54 +0600

    change auth at firebase init file

--- src/components/firebase/firebase.init.js
+++ src/components/firebase/firebase.init.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import firebaseConfig from "./firebase.config";
 
-const initializeAuthentication = () => {
-  initializeApp(firebaseConfig);
-};
-
-export default initializeAuthentication;
+// const initializeAuthentication = () => {
+//   initializeApp(firebaseConfig);
+// };
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
